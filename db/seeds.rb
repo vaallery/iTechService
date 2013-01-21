72453eb1c3ab28ee3a8781854e0c3d1f6a205241
kvn47 <kvn@itechdevs.com>
2013-01-21 15:35:27 +1100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,52 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: 'admin', email: 'admin@itechstore.foo', role: 'admin', password: 'azaexe', password_confirmation: 'azaexe')
-
-DeviceType.create [
-  {name: "iPhone 4 16GB"},
-  {name: "iPhone 4 32GB"},
-  {name: "iPhone 5 64GB"},
-  {name: "iMac 27"},
-  {name: "MacBook Air"},
-  {name: "MacBook Pro"}
-]
-
-Task.create [
-  {
-    name: "Update",
-    priority: rand(10),
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  },
-  {
-    name: "Install soft",
-    priority: rand(10),
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  },
-  {
-    name: "Repair",
-    priority: 8,
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  },
-  {
-    name: "Install media",
-    priority: rand(10),
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  },
-  {
-    name: "Jailbreak",
-    priority: rand(10),
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  },
-  {
-    name: "Unlock",
-    priority: rand(10),
-    cost: rand(5)*1000,
-    duration: rand(20)*10
-  }
-]
+User.create(username: 'admin', email: 'admin@itechstore.foo', role: 'admin',
+            password: 'wrfswrfs', password_confirmation: 'wrfswrfs', card_number: '1b12abe8')
