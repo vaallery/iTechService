70008e964295e63fa3c07afcdb284eec134ce8ae
kvn47 <kvn@itechdevs.com>
2013-02-14 01:29:37 +1100

    client devices

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,8 +5,6 @@ class Location < ActiveRecord::Base
   has_ancestry
   attr_accessible :name, :ancestry, :parent_id, :schedule
   default_scope order('ancestry asc')
-  #scope :allowed_for, lambda { |user| where("ancestry LIKE ? OR ancestry is NULL",
-  #                                          "#{user.location.ancestor_ids.join('/')}%") }
   scope :for_schedule, where(schedule: true)
 
   def full_name
