9c4c9ed6973e120564b8df10a4e587a8b8cfe4de
kvn47 <kvn@itechdevs.com>
2013-03-06 12:40:09 +1100

    f

--- app/inputs/replaced_input.rb
+++ app/inputs/replaced_input.rb
@@ -5,7 +5,7 @@ class ReplacedInput < SimpleForm::Inputs::BooleanInput
     template.content_tag(:span, class: 'help-inline') do
       template.t('device_types.available_for_replacement').html_safe + ': ' +
       template.content_tag(:span, id: 'available_for_replacement') do
-        @builder.object.device_type.available_for_replacement unless @builder.object.new_record?
+        @builder.object.device_type.try(:available_for_replacement) unless @builder.object.new_record?
       end
     end).html_safe
   end
