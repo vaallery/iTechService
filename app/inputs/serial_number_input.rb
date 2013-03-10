61839921f0f9c6bad3f8422974113a7277785144
kvn47 <kvn@itechdevs.com>
2013-03-10 16:42:23 +1100

    f

--- app/inputs/serial_number_input.rb
+++ app/inputs/serial_number_input.rb
@@ -3,7 +3,7 @@ class SerialNumberInput < SimpleForm::Inputs::StringInput
   def input
     t = template
     t.content_tag(:div, class: 'input-append') do
-      @builder.input_field(attribute_name, class: 'input-large') +
+      @builder.input_field(attribute_name, class: 'input-large', autocomplete: 'off') +
       t.link_to(t.icon_tag('search'), '#', id: 'device_serial_number_search', class: 'btn btn-info', remote: true,
       title: t.t('devices.seach_sales_btn'))
     end.html_safe
