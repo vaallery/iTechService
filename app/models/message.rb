82312f1297a4c02c48e44131b7be20bb11afb09e
kvn47 <kvn@itechdevs.com>
2013-03-13 15:39:36 +1100

    f

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ActiveRecord::Base
   belongs_to :recipient, polymorphic: true
   attr_accessible :content, :recipient_id, :recipient_type
   validates :content, presence: true
+  validates :content, length: {maximum: 255}
   scope :newest, order("messages.created_at desc")
   scope :today, lambda { where('created_at > ?', Time.current.beginning_of_day) }
   before_validation do |message|
