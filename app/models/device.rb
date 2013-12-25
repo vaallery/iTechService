79ebddff50619d8e386c7b790e7c0b30701ff176
kvn47 <kvn@itechdevs.com>
2013-12-25 13:51:54 +1100

    f

--- app/models/device.rb
+++ app/models/device.rb
@@ -18,7 +18,6 @@ class Device < ActiveRecord::Base
   validates_presence_of :app_store_pass, if: :new_record?
   validates_uniqueness_of :ticket_number
   validates_length_of :imei, is: 15, allow_blank: true
-  #validates :service_duration, format: { with: /[\D.]+\z/ }
   validates_associated :device_tasks
   
   before_validation :generate_ticket_number
