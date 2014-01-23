b514705bd2e38095254a7b15d2361a2adbc11f98
kvn47 <kvn@itechdevs.com>
2014-01-23 14:04:46 +1100

    p

--- app/models/product_group.rb
+++ app/models/product_group.rb
@@ -1,11 +1,16 @@
 class ProductGroup < ActiveRecord::Base
 
+  scope :services, joins(:product_category).where(product_categories: {kind: 'service'})
+  scope :goods, joins(:product_category).where(product_categories: {kind: %w[equipment accessory]})
+
   belongs_to :product_category
   has_many :products, inverse_of: :product_group
   has_ancestry orphan_strategy: :restrict, cache_depth: true
+
+  delegate :feature_accounting, :feature_types, :warranty_term, :is_service, to: :product_category, allow_nil: true
+
   attr_accessible :name, :ancestry, :parent_id, :product_category_id
   validates_presence_of :name, :product_category
-  delegate :feature_accounting, :feature_types, :warranty_term, :is_service, to: :product_category, allow_nil: true
 
   after_initialize do |product_group|
     product_group.parent_id = nil if product_group.parent_id.blank?
@@ -14,8 +19,5 @@ class ProductGroup < ActiveRecord::Base
     end
 
   end
-
-  scope :services, joins(:product_category).where(product_categories: {kind: 'service'})
-  scope :goods, joins(:product_category).where(product_categories: {kind: %w[equipment accessory]})
 
 end
