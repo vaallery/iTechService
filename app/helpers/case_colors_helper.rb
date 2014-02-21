527ad4a718d0a3838f3e176225b5152cf3b94bfe
kvn47 <kvn@itechdevs.com>
2014-02-21 15:47:52 +1100

    case_color

--- app/helpers/case_colors_helper.rb
+++ app/helpers/case_colors_helper.rb
@@ -3,7 +3,7 @@ module CaseColorsHelper
   def case_color_presentation(case_color)
     if case_color.present?
       content_tag(:span, class: 'case_color') do
-        content_tag(:div, nil, class: 'color_icon', style: "background-color: #{case_color.color}") +
+        content_tag(:div, nil, class: 'color_icon', style: "background-color: #{case_color.color || 'transparent'}") +
         case_color.name.html_safe
       end
     else
