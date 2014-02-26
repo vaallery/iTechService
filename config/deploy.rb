cf7cc1bdca32aacd084c81f6780137bb9415234f
kvn47 <kvn@itechdevs.com>
2014-02-26 15:55:06 +1100

    d

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
-set :rvm_ruby_version, '2.1.1'
+set :rvm_ruby_version, '1.9.3-p545'
 
 set :sockets_path, shared_path.join('tmp/sockets')
 set :pids_path, shared_path.join('tmp/pids')
