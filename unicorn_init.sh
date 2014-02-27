d0f3e24e271f4dcd38d4e398a1519d7e9de75e3c
kvn47 <kvn@itechdevs.com>
2014-02-27 16:21:01 +1100

    d

--- unicorn_init.sh
+++ unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/usr/local/var/www/itechservice/current
 #APP_ROOT=/Users/itech/sites/itech_service
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -c $APP_ROOT/config/unicorn.rb -E production -D"
+CMD="cd $APP_ROOT; bundle exec unicorn -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=itech
 set -u
 
