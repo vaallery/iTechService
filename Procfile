563d5f727d96a182d13cb2b977d3d478945292b1
kvn47 <kvn@itechdevs.com>
2014-03-08 16:14:27 +1100

    d

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-faye: rackup private_pub.ru -s thin -p $PORT -e $RACK_ENV
+web: bundle exec unicorn -c /usr/local/var/www/itechservice/current/config/unicorn.rb -E production
+job: bundle exec script/delayed_job start RAILS_ENV=production
+faye: bundle exec rackup private_pub.ru -s thin -E production
--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,100 @@ namespace :deploy do
 
 end
 
+namespace :foreman do
+  desc 'Start server'
+  task :start do
+    on roles(:all) do
+      sudo "start #{application}"
+    end
+  end
+
+  desc 'Stop server'
+  task :stop do
+    on roles(:all) do
+      sudo "stop #{application}"
+    end
+  end
+
+  desc 'Restart server'
+  task :restart do
+    on roles(:all) do
+      sudo "restart #{application}"
+    end
+  end
+
+  desc 'Server status'
+  task :status do
+    on roles(:all) do
+      execute "launchctl list | grep #{application}"
+    end
+  end
+end
+
+desc 'Setup'
+task :setup do
+  on roles(:all) do
+    deploy_path = fetch :deploy_to
+    execute "mkdir #{shared_path}/config/"
+    #execute "mkdir #{shared_path}/system"
+    upload!('shared/database.yml', "#{shared_path}/config/database.yml")
+    upload!('shared/Procfile', "#{shared_path}/Procfile")
+    upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
+    sudo 'start nginx'
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, 'db:create'
+      end
+    end
+  end
+end
+
+desc 'Create symlink'
+task :symlink do
+  on roles(:all) do
+    execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    execute "ln -s #{shared_path}/Procfile #{release_path}/Procfile"
+  end
+end
+
+desc 'Foreman init'
+task :foreman_init do
+  on roles(:all) do
+    foreman_temp = "/usr/local/var/www/tmp/foreman"
+    execute "mkdir -p #{foreman_temp}"
+    execute "ln -s #{release_path} #{current_path}"
+
+    within current_path do
+      execute "cd #{current_path}"
+      execute :bundle, "exec foreman export launchd #{foreman_temp} -a #{application} -u itech -l #{shared_path}/log -d #{current_path}"
+    end
+    sudo "mv #{foreman_temp}/* /Library/LaunchDaemons/"
+    sudo "rm -r #{foreman_temp}"
+  end
+end
+
+desc 'Restart application'
+task :restart do
+  on roles(:app), in: :sequence, wait: 5 do
+    sudo "restart #{application}"
+  end
+end
+
+after :finishing, 'deploy:cleanup'
+after :finishing, 'deploy:restart'
+
+after :updating, 'deploy:symlink'
+
+after :setup, 'deploy:foreman_init'
+
+after :foreman_init, 'foreman:start'
+
+before :foreman_init, 'rvm:hook'
+
+before :setup, 'deploy:starting'
+before :setup, 'deploy:updating'
+before :setup, 'bundler:install'
+
 #before 'deploy:updated', 'deploy:setup_config'
 #after 'deploy:started', 'deploy:setup_config'
 #after 'deploy:updated', 'deploy:symlink_config'
