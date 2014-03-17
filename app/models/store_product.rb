63e76626f1dce7418ab94e265791cb80b12db419
kvn47 <kvn@itechdevs.com>
2014-03-17 18:08:06 +1100

    f

--- app/models/store_product.rb
+++ app/models/store_product.rb
@@ -3,7 +3,7 @@ class StoreProduct < ActiveRecord::Base
   belongs_to :store
   belongs_to :product
   attr_accessible :warning_quantity, :store_id, :product_id
-  validates_presence_of :store, :product
+  validates_presence_of :store
   validates_numericality_of :warning_quantity, only_integer: true, greater_than: 0, allow_nil: true
   validates_uniqueness_of :product_id, scope: :store_id
 
