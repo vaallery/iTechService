560a92e783d38ab564b456fce0c827d3e22197ee
kvn47 <kvn@itechdevs.com>
2014-03-19 11:36:43 +1100

    fix imei validation in device

--- app/models/device.rb
+++ app/models/device.rb
@@ -36,7 +36,7 @@ class Device < ActiveRecord::Base
   validates_presence_of :device_type, if: 'item.nil?'
   validates_presence_of :app_store_pass, if: :new_record?
   validates_uniqueness_of :ticket_number
-  validates :imei, length: {is: 15}, allow_nil: true
+  validates :imei, length: {is: 15}, allow_nil: true unless 'imei.blank?'
   validates_associated :device_tasks
   validate :presence_of_payment
   before_validation :generate_ticket_number
