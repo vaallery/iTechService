d6c13496440d0403d6bab60624255a3bf9b875b7
kvn47 <kvn@itechdevs.com>
2014-03-24 16:04:39 +1100

    fix

--- app/models/repair_service.rb
+++ app/models/repair_service.rb
@@ -1,6 +1,6 @@
 class RepairService < ActiveRecord::Base
   belongs_to :repair_group
-  has_many :spare_parts
+  has_many :spare_parts, dependent: :destroy
   has_many :store_items, through: :spare_parts, uniq: true
   accepts_nested_attributes_for :spare_parts
   attr_accessible :name, :price, :client_info, :repair_group_id, :spare_parts_attributes
