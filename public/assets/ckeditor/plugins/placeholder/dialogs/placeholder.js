29059be2156199fea7d9b12cb816e5c00852e80f
kvn47 <kvn@itechdevs.com>
2014-03-24 12:22:23 +1100

    assets

---
+++ public/assets/ckeditor/plugins/placeholder/dialogs/placeholder.js
@@ -0,0 +1,5 @@
+/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+!function(){function e(e,t){var a=e.lang.placeholder,n=e.lang.common.generalTab;return{title:a.title,minWidth:300,minHeight:80,contents:[{id:"info",label:n,title:n,elements:[{id:"text",type:"text",style:"width: 100%;",label:a.text,"default":"",required:!0,validate:CKEDITOR.dialog.validate.notEmpty(a.textMissing),setup:function(e){t&&this.setValue(e.getText().slice(2,-2))},commit:function(t){var a="[["+this.getValue()+"]]";CKEDITOR.plugins.placeholder.createPlaceholder(e,t,a)}}]}],onShow:function(){t&&(this._element=CKEDITOR.plugins.placeholder.getSelectedPlaceHoder(e)),this.setupContent(this._element)},onOk:function(){this.commitContent(this._element),delete this._element}}}CKEDITOR.dialog.add("createplaceholder",function(t){return e(t)}),CKEDITOR.dialog.add("editplaceholder",function(t){return e(t,1)})}();
