25e3c435010cf9f362a6c6bb985cb04be23528a7
kvn47 <kvn@itechdevs.com>
2014-04-24 13:54:57 +1100

    dep

--- db/migrate/20140217055436_add_department_id_to_users.rb
+++ db/migrate/20140217055436_add_department_id_to_users.rb
@@ -2,5 +2,9 @@ class AddDepartmentIdToUsers < ActiveRecord::Migration
   def change
     add_column :users, :department_id, :integer
     add_index :users, :department_id
+
+    User.unscoped.find_each do |r|
+      r.update_column :department_id, Department.current.id
+    end
   end
 end
