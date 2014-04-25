e9dd28cd2f061f3c7814a461aad9508566debc89
kvn47 <kvn@itechdevs.com>
2014-04-25 12:06:23 +1100

    c

--- app/pdfs/case_picture_pdf.rb
+++ app/pdfs/case_picture_pdf.rb
@@ -86,33 +86,6 @@ class CasePicturePdf < Prawn::Document
     @x5, @x6 = @x5 - contour, @x6 + contour
     @y5, @y6 = @y5 + contour*8, @y6 - contour*8
     draw_shape
-    # # left side
-    # line [@x1, @y5], [@x1, @y3]
-    # line_to @x3, @y3
-    # line_to @x3, @y4
-    # line_to @x1, @y4
-    # line_to @x1, @y6
-    #
-    # # bottom side
-    # curve_to [@x5, @y2], bounds: [[@x1+1.mm, @y2], [@x5, @y2]]
-    # line_to @x5, @y2 - 2.mm
-    # line_to @x6, @y2 - 2.mm
-    # line_to @x6, @y2
-    # curve_to [@x2, @y6], bounds: [[@x6, @y2], [@x2-1.mm, @y2]]
-    #
-    # # right side
-    # line_to @x2, @y4
-    # line_to @x4, @y4
-    # line_to @x4, @y3
-    # line_to @x2, @y3
-    # line_to @x2, @y5
-    #
-    # # top side
-    # curve_to [@x6, @y1], bounds: [[@x2, @y5], [@x2, @y1-1.mm]]
-    # line_to @x6, @y1 + 2.mm
-    # line_to @x5, @y1 + 2.mm
-    # line_to @x5, @y1
-    # curve_to [@x1, @y5], bounds: [[@x5, @y1], [@x1+1.mm, @y1]]
   end
 
 end
