6b4b6d1ed875ca109db6b53c6686468421938052
kvn47 <kvn@itechdevs.com>
2014-04-30 13:03:19 +1100

    add username uniqueness validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,6 +74,7 @@ class User < ActiveRecord::Base
   attr_accessible :auth_token, :login, :username, :email, :role, :password, :password_confirmation, :remember_me, :location_id, :surname, :name, :patronymic, :position, :birthday, :hiring_date, :salary_date, :prepayment, :wish, :photo, :remove_photo, :photo_cache, :schedule_days_attributes, :duty_days_attributes, :card_number, :color, :karmas_attributes, :abilities, :schedule, :is_fired, :job_title, :position, :salaries_attributes, :installment_plans_attributes, :installment, :department_id, :session_duration
 
   validates_presence_of :username, :role, :department
+  validates_uniqueness_of :username
   validates :password, presence: true, confirmation: true, if: :password_required?
   validates :role, inclusion: { in: ROLES }
   validates_numericality_of :session_duration, only_integer: true, greater_than: 0, allow_nil: true
