6ba925b05801c67a148cabccda3b4bb78560b1fa
kvn47 <kvn@itechdevs.com>
2014-05-29 13:13:29 +1100

    d

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,7 +18,7 @@ set :branch, 'master'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-# server '192.168.0.1', user: 'itech', roles: %w{web app db}
+server '192.168.0.1', user: 'itech', roles: %w{web app db}
 server '192.168.4.200', user: 'itech', roles: %w{web app db}
 
 # you can set custom ssh options
