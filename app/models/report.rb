7b54804cd7702fe49ee2193d692d62b25a8739fe
kvn47 <kvn@itechdevs.com>
2014-09-04 14:18:56 +1100

    report repair jobs without payment

--- app/models/report.rb
+++ app/models/report.rb
@@ -361,6 +361,7 @@ class Report
     # result['with_parts'] = result['without_parts'] = {}
     result.store :with_parts, {}
     result.store :without_parts, {}
+    result.store :without_payment, {}
     repair_tasks.each do |repair_task|
       if repair_task.repair_service.present?
         repair_group_id = (repair_task.repair_group.try(:id) || '-').to_s
@@ -368,20 +369,23 @@ class Report
         repair_service_id = repair_task.repair_service_id || '-'
         repair_service_name = repair_task.name || '-'
         job = {id: repair_task.id, price: repair_task.price, parts_cost: repair_task.parts_cost, margin: repair_task.margin, device_id: repair_task.device.id, device_presentation: repair_task.device.presentation}
-        group = repair_task.repair_parts.count > 0 ? :with_parts : :without_parts
-        if result[group][repair_group_id].present?
-          if result[group][repair_group_id][:services][repair_service_id].is_a? Hash
-            result[group][repair_group_id][:jobs_qty] = result[group][repair_group_id][:jobs_qty] + 1
-            result[group][repair_group_id][:services][repair_service_id][:jobs_qty] = result[group][repair_group_id][:services][repair_service_id][:jobs_qty] + 1
-            result[group][repair_group_id][:services][repair_service_id][:jobs_sum] = result[group][repair_group_id][:services][repair_service_id][:jobs_sum] + repair_task.margin
-            result[group][repair_group_id][:services][repair_service_id][:jobs] << job
+        groups = [repair_task.repair_parts.count > 0 ? :with_parts : :without_parts]
+        groups << :without_payment unless repair_task.price > 0
+        groups.each do |group|
+          if result[group][repair_group_id].present?
+            if result[group][repair_group_id][:services][repair_service_id].is_a? Hash
+              result[group][repair_group_id][:jobs_qty] = result[group][repair_group_id][:jobs_qty] + 1
+              result[group][repair_group_id][:services][repair_service_id][:jobs_qty] = result[group][repair_group_id][:services][repair_service_id][:jobs_qty] + 1
+              result[group][repair_group_id][:services][repair_service_id][:jobs_sum] = result[group][repair_group_id][:services][repair_service_id][:jobs_sum] + repair_task.margin
+              result[group][repair_group_id][:services][repair_service_id][:jobs] << job
+            else
+              result[group][repair_group_id][:jobs_qty] = result[group][repair_group_id][:jobs_qty] + 1
+              result[group][repair_group_id][:services_qty] = result[group][repair_group_id][:services_qty] + 1
+              result[group][repair_group_id][:services][repair_service_id] = {name: repair_service_name, jobs_qty: 1, jobs_sum: repair_task.margin, jobs: [job]}
+            end
           else
-            result[group][repair_group_id][:jobs_qty] = result[group][repair_group_id][:jobs_qty] + 1
-            result[group][repair_group_id][:services_qty] = result[group][repair_group_id][:services_qty] + 1
-            result[group][repair_group_id][:services][repair_service_id] = {name: repair_service_name, jobs_qty: 1, jobs_sum: repair_task.margin, jobs: [job]}
+            result[group][repair_group_id] = {name: repair_group_name, services_qty: 1, jobs_qty: 1, services: {repair_service_id => {name: repair_service_name, jobs_qty: 1, jobs_sum: repair_task.margin, jobs: [job]}}}
           end
-        else
-          result[group][repair_group_id] = {name: repair_group_name, services_qty: 1, jobs_qty: 1, services: {repair_service_id => {name: repair_service_name, jobs_qty: 1, jobs_sum: repair_task.margin, jobs: [job]}}}
         end
       end
     end
