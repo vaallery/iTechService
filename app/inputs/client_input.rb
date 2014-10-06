8e69ecb4d8ba63ecb7244dc8b739e800f983ad73
kvn47 <kvn@itechdevs.com>
2014-10-06 12:33:40 +1100

    fix client input

--- app/inputs/client_input.rb
+++ app/inputs/client_input.rb
@@ -3,8 +3,13 @@ class ClientInput < SimpleForm::Inputs::StringInput
   def input
     (
     template.content_tag(:div, id: 'client_input', class: 'input-prepend input-append') do
+      client = @builder.object.try(:client)
       template.content_tag(:span, template.icon_tag(:search), class: 'add-on') +
-      template.text_field_tag(:client_search, @builder.object.try(:client).try(:presentation), placeholder: template.t('client_input_placeholder').html_safe, autofocus: true, autocomplete: 'off', style: "color: #{@builder.object.try(:client).try(:category_color)}", class: 'has-tooltip', title: @builder.object.try(:client).try(:characteristic), data: {html: true, container: 'body'}) +
+      if client.is_a?(Client)
+        template.text_field_tag(:client_search, client.try(:presentation), placeholder: template.t('client_input_placeholder').html_safe, autofocus: true, autocomplete: 'off', style: "color: #{@builder.object.try(:client).try(:category_color)}", class: 'has-tooltip', title: @builder.object.try(:client).try(:characteristic), data: {html: true, container: 'body'})
+      else
+        template.text_field_tag(:client_search, client.try(:presentation), placeholder: template.t('client_input_placeholder').html_safe, autofocus: true, autocomplete: 'off')
+      end +
       @builder.hidden_field(attribute_name) +
       template.link_to(template.icon_tag(:plus), template.new_client_path, id: 'new_client_link', class: 'btn', remote: true) +
       template.link_to(template.icon_tag(:edit), @builder.object.client.present? ? template.edit_client_path(@builder.object.client) : '#', id: 'edit_client_link', class: 'btn', remote: @builder.object.client.present?)
