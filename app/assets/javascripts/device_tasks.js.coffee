d98eec3ef4c007b1542d995c1b8b13929f95b372
kvn47 <kvn@itechdevs.com>
2014-10-11 15:20:38 +1100

    fix device_task done

--- app/assets/javascripts/device_tasks.js.coffee
+++ app/assets/javascripts/device_tasks.js.coffee
@@ -1,5 +1,4 @@
-$(document).on 'click', '.done_status>button', (event)->
+$(document).on 'click', '.done_status>a', (event)->
   $input = $(this).closest('.done_status').next('input')
   $input.val($(this).data('value'))
-  console.log $input.val()
   event.preventDefault()
--- app/inputs/done_status_input.rb
+++ app/inputs/done_status_input.rb
@@ -2,9 +2,9 @@ class DoneStatusInput < SimpleForm::Inputs::Base
 
   def input
     template.content_tag(:div, class: 'done_status btn-group', 'data-toggle' => 'buttons-radio') do
-      template.button_tag(template.icon_tag('check-empty'), 'data-value' => 0, class: "btn #{'active' if @builder.object.pending?}") +
-      template.button_tag(template.icon_tag('check'), 'data-value' => 1, class: "btn #{'active' if @builder.object.done?}") +
-      template.button_tag(template.icon_tag('check-minus'), 'data-value' => 2, class: "btn #{'active' if @builder.object.undone?}")
+      template.link_to(template.icon_tag('check-empty'), '#', 'data-value' => 0, class: "btn #{'active' if @builder.object.pending?}") +
+      template.link_to(template.icon_tag('check'), '#', 'data-value' => 1, class: "btn #{'active' if @builder.object.done?}") +
+      template.link_to(template.icon_tag('check-minus'), '#', 'data-value' => 2, class: "btn #{'active' if @builder.object.undone?}")
     end +
     @builder.hidden_field(attribute_name)
   end
