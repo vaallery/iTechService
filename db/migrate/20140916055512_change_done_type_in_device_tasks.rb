e6c29d12c933ec7205740a4fd2dea89e72623f8a
kvn47 <kvn@itechdevs.com>
2014-10-24 16:15:52 +1100

    m

--- db/migrate/20140916055512_change_done_type_in_device_tasks.rb
+++ db/migrate/20140916055512_change_done_type_in_device_tasks.rb
@@ -1,6 +1,6 @@
 class ChangeDoneTypeInDeviceTasks < ActiveRecord::Migration
   def up
-    change_column :device_tasks, :done, 'integer USING CAST(done AS integer)', limit: 1, default: 0
+    change_column :device_tasks, :done, 'integer USING CAST(done AS integer)', limit: 1
     change_column_default :device_tasks, :done, 0
   end
 
