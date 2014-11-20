159594d630eb8dae47a67e0c6aaf6a28216748ed
kvn47 <kvn@itechdevs.com>
2014-11-20 16:15:49 +1000

    add order priority validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -30,6 +30,7 @@ class Order < ActiveRecord::Base
 
   attr_accessible :customer_id, :customer_type, :comment, :desired_date, :object, :object_kind, :status, :user_id, :user_comment, :department_id, :quantity, :priority
   validates :customer, :department, :quantity, :object, :object_kind, presence: true
+  validates :priority, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
   after_initialize { self.department_id ||= Department.current.id }
   before_validation :generate_number
 
