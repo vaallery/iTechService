967272035a585cd84e301cc043449d271d0d0af3
Vladimir Kochergin <kvn124@gmail.com>
2015-12-18 13:29:39 +1000

    config deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,9 +17,9 @@ set :branch, 'master'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '192.168.0.1', user: 'itech', roles: %w{web app db}
-# server '192.168.4.200', user: 'itech', roles: %w{web app db}
-# server '192.168.9.180', user: 'deployer', roles: %w{web app db}
+server 'ise.vl', user: 'itech', roles: %w{web app db}
+server 'ise.kh', user: 'itech', roles: %w{web app db}
+server 'ise.sah', user: 'deployer', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
