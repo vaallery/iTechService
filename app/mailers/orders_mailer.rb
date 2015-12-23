56462efe02c1f41562aea62804ee54ae4bbcec87
Vladimir Kochergin <kvn124@gmail.com>
2015-12-23 12:54:08 +1000

    Fix mailers

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,6 @@
 class OrdersMailer < ActionMailer::Base
   def notice(order_id)
     @order = Order.find order_id
-    mail to: 'kvn@itechdevs.com, kolesnik@itech.pw', subject: t('mail.orders.new')
+    mail to: 'kolesnik@itech.pw, oleinikov@itech.pw', subject: t('mail.orders.new')
   end
 end
