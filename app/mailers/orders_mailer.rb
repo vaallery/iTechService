bc6774440995efa3c6eecefcf9fb6fefffe5177d
Vladimir Kochergin <kvn124@gmail.com>
2015-12-23 13:02:22 +1000

    orders mailer

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,6 @@
 class OrdersMailer < ActionMailer::Base
   def notice(order_id)
     @order = Order.find order_id
-    mail to: 'kolesnik@itech.pw, oleinikov@itech.pw', subject: t('mail.orders.new')
+    mail to: 'kolesnik@itech.pw, popov@itech.pw, oleinikov@itech.pw, shamaev@itech.pw', subject: t('mail.orders.new')
   end
 end
