fba82a38689cd322d2737aaf923751d8f83d2fb0
Vladimir Kochergin <kvn124@gmail.com>
2016-01-12 11:59:24 +1000

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,18 +17,18 @@ set :branch, 'master'
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'ise.vl', user: 'itech', roles: %w{web app db}
-server 'ise.kh', user: 'itech', roles: %w{web app db}
-server 'ise.sah', user: 'deployer', roles: %w{web app db}
+server 'ise.vl', user: 'itech', roles: %w[web app db]
+server 'ise.kh', user: 'itech', roles: %w[web app db]
+server 'ise.sah', user: 'deployer', roles: %w[web app db]
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
 set :ssh_options, {
-  keys: %w(/Users/vova/.ssh/id_rsa),
+  keys: %w[/Users/vova/.ssh/id_rsa],
   forward_agent: true,
-  auth_methods: %w(publickey)
+  auth_methods: %w[publickey]
 }
 # and/or per server
 # server 'example.com',
