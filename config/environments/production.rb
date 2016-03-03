af2f3a0a8017e7a2f611aaef255c1f201b50abf3
Vladimir Kochergin <kvn124@gmail.com>
2016-03-03 13:00:09 +1000

    Assets settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,8 @@ ItechService::Application.configure do
   # config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
-  #config.assets.compress = false
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   #config.assets.compile = false
@@ -22,19 +22,6 @@ ItechService::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  #config.assets.initialize_on_precompile = false
-  #config.assets.initialize_on_precompile = true
-
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
 
   # See everything in the log (default is :info)
   config.log_level = :debug
