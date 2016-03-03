185c02c2b7a52b98648bf54c7a1369360cc10ff4
Vladimir Kochergin <kvn124@gmail.com>
2016-03-03 12:08:03 +1000

    Modify SalesImportJob for new export format

--- app/models/sales_import_job.rb
+++ app/models/sales_import_job.rb
@@ -14,19 +14,21 @@ class SalesImportJob < Struct.new(:params)
       9.upto sheet.last_row-1 do |r|
         row = sheet.row r
         import_logs << ['inverse', "#{r} #{'-'*140}"]
-        case row_type row[0]
+        case row_type row[0], row[1]
           when :device_type
-            code_1c = RGXP_DEVICE_TYPE.match(row[0])[1].to_i
-            device_type_s = RGXP_DEVICE_TYPE.match(row[0])[2]
+            # code_1c = RGXP_PRODUCT_CODE_AND_NAME.match(row[0])[1].to_i
+            # device_type_s = RGXP_PRODUCT_CODE_AND_NAME.match(row[0])[2]
+            code_1c = row[0]
+            device_type_s = row[1]
             unless (device_type = DeviceType.find_by_code_1c code_1c).present?
               import_logs << ['important', "Device type '#{device_type_s}' [#{code_1c}] not found!!!"]
             end
           when :device_1
-            sn = RGXP_DEVICE_1.match(row[0])[1]
+            sn = RGXP_SERIAL_NUMBER.match(row[0])[1]
             import_logs << ['info', row[0]]
           when :device_2
-            sn = RGXP_DEVICE_2.match(row[0])[1]
-            imei = RGXP_DEVICE_2.match(row[0])[2]
+            sn = RGXP_SERIAL_NUMBER_IMEI.match(row[0])[1]
+            imei = RGXP_SERIAL_NUMBER_IMEI.match(row[0])[2]
             import_logs << ['info', row[0]]
           when :date
             date = RGXP_DATE.match(row[0])[1]
@@ -40,7 +42,7 @@ class SalesImportJob < Struct.new(:params)
               end
             end
           else
-            # type code here
+
         end
       end
       import_logs << ['inverse', '-'*160]
@@ -58,54 +60,19 @@ class SalesImportJob < Struct.new(:params)
 
   private
 
-  RGXP_DEVICE_TYPE = Regexp.new '(\d+) \| (.+)'
-  RGXP_DEVICE_1 = Regexp.new '\A(\w+)\Z'
-  RGXP_DEVICE_2 = Regexp.new '(\w+), (\d+)'
-  RGXP_DATE = Regexp.new '(\d{2}.\d{2}.\d{4}\ \d{,2}:\d{2}:\d{2})'
+  RGXP_PRODUCT_CODE_AND_NAME = Regexp.new '(\d+) \| (.+)'
+  RGXP_PRODUCT_CODE = Regexp.new '\d+'
+  RGXP_PRODUCT_NAME = Regexp.new '.+'
+  RGXP_SERIAL_NUMBER = Regexp.new '\A(\w+)\Z'
+  RGXP_SERIAL_NUMBER_IMEI = Regexp.new '(\w+), (\d+)'
+  RGXP_DATETIME = Regexp.new '(\d{2}.\d{2}.\d{4}\ \d{,2}:\d{2}:\d{2})'
+  RGXP_DATE = Regexp.new '(\d{2}.\d{2}.\d{4})'
 
-  def load_imported_sales
-    sheet = open_file
-    sales = []
-    code_1c = device_type_s = sn = imei = date = qty = ''
-    device_type = nil
-    9.upto sheet.last_row-1 do |r|
-      row = sheet.row r
-      import_logs << ['inverse', "#{r} #{'-'*140}"]
-      case row_type row[0]
-        when :device_type
-          code_1c = RGXP_DEVICE_TYPE.match(row[0])[1].to_i
-          device_type_s = RGXP_DEVICE_TYPE.match(row[0])[2]
-          unless (device_type = DeviceType.find_by_code_1c code_1c).present?
-            import_logs << ['important', "Device type '#{device_type_s}' [#{code_1c}] not found!!!"]
-          end
-        when :device_1
-          sn = RGXP_DEVICE_1.match(row[0])[1]
-          import_logs << ['info', row[0]]
-        when :device_2
-          sn = RGXP_DEVICE_2.match(row[0])[1]
-          imei = RGXP_DEVICE_2.match(row[0])[2]
-          import_logs << ['info', row[0]]
-        when :date
-          date = RGXP_DATE.match(row[0])[1]
-          qty = row[3].to_i
-          sale = ImportedSale.find_or_initialize_by_serial_number serial_number: sn, imei: imei, sold_at: date.to_date, device_type_id: device_type.try(:id), quantity: qty
-          unless !sale.new_record? and sale.sold_at == date.to_date
-            sales << sale
-            import_logs << ['success', "#{sale.new_record? ? 'New' : 'Existing'} device #{device_type.try(:full_name)} [#{sale.serial_number}] sold_at: #{sale.sold_at}"]
-          end
-        else
-          # type code here
-      end
-    end
-    import_logs << ['inverse', '-'*160]
-    sales
-  end
-
-  def row_type(val)
-    case val
-      when RGXP_DEVICE_TYPE then :device_type
-      when RGXP_DEVICE_1 then :device_1
-      when RGXP_DEVICE_2 then :device_2
+  def row_type(val1, val2)
+    case val1
+      when RGXP_PRODUCT_CODE then RGXP_PRODUCT_NAME === val2 ? :device_type : nil
+      when RGXP_SERIAL_NUMBER then :device_1
+      when RGXP_SERIAL_NUMBER_IMEI then :device_2
       when RGXP_DATE then :date
       else nil
     end
