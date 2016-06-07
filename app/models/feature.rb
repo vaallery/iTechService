b68de320cd7b9fc70e407cb9508eb90bf1933cfb
Vladimir Kochergin <kvn124@gmail.com>
2016-06-07 23:01:01 +1000

    Add Feature validation conditions
    Allow enter '?' value

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -7,8 +7,8 @@ class Feature < ActiveRecord::Base
   belongs_to :item, inverse_of: :features
   attr_accessible :value, :item, :item_id, :feature_type, :feature_type_id
   validates_presence_of :value, :feature_type
-  validates_uniqueness_of :value, scope: [:feature_type_id]
-  validates_length_of :value, is: 15, if: :is_imei?
+  validates_uniqueness_of :value, scope: [:feature_type_id], unless: Proc.new { |feature| feature.value == '?' }
+  validates_length_of :value, is: 15, if: Proc.new { |feature| feature.is_imei? && feature.value != '?' }
   delegate :name, :kind, :is_imei?, to: :feature_type, allow_nil: true
 
   default_scope {order('feature_type_id asc')}
