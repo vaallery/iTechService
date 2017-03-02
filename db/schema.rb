ee10972b1c55809d2dc80b07f0a9ab9d4184b414
Владимир Кочергин <vova@macbookv.local>
2017-03-02 20:56:04 +1000

    Hiding locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221042422) do
+ActiveRecord::Schema.define(version: 20170302074458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -492,12 +492,13 @@ ActiveRecord::Schema.define(version: 20170221042422) do
   create_table "locations", force: :cascade do |t|
     t.string   "name",          limit: 255
     t.string   "ancestry",      limit: 255
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.boolean  "schedule"
     t.integer  "position",                  default: 0
     t.string   "code",          limit: 255
     t.integer  "department_id"
+    t.boolean  "hidden",                    default: false
   end
 
   add_index "locations", ["ancestry"], name: "index_locations_on_ancestry", using: :btree
