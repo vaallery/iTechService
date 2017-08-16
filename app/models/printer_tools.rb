b55874f0ed88ffe9338b410c9b35ec835136d037
Владимир Кочергин <vova@MacBook-Pro.kairos>
2017-08-16 23:29:42 +1000

    Fix

--- app/models/printer_tools.rb
+++ app/models/printer_tools.rb
@@ -3,7 +3,7 @@ module PrinterTools
   def self.print_file(filepath, type: nil, height: nil, printer: nil)
     if File.exists? filepath
       options = '-o PageCutType=1PartialCutPage'
-      case type.to_sym
+      case type&.to_sym
         when :sale_check then options << " -o media=Custom.72x#{height || 200}mm"
         when :ticket then options << ' -o media=Custom.72x90mm'
         when :quick_order then options << ' -o media=Custom.72x90mm'
