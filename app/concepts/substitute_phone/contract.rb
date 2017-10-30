742f7c4996168b9ca0197fd715df0778508558c6
Vladimir Kochergin <kvn047@gmail.com>
2017-10-30 21:42:06 +1000

    Fix changing department of SubstitutePhone

--- app/concepts/substitute_phone/contract.rb
+++ app/concepts/substitute_phone/contract.rb
@@ -1,7 +1,8 @@
 class SubstitutePhone < ApplicationRecord
   class Contract < BaseForm
     model SubstitutePhone
-    properties :item, :item_id, :department, :department_id, :condition
+    properties :item, :department, writeable: false
+    properties :item_id, :department_id, :condition
     validates :item_id, :department_id, :condition, presence: true
   end
 end
