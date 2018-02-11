0297a42b6ebf86051ae24e2a6aff4682bd430f59
Vladimir Kochergin <kvn047@gmail.com>
2018-02-11 22:00:43 +1000

    Show user his faults.

--- app/concepts/fault/operation/index.rb
+++ app/concepts/fault/operation/index.rb
@@ -1,6 +1,10 @@
 class Fault::Index < BaseOperation
-  step Policy::Pundit(Fault::Policy, :index?)
-  step ->(options, params:, **) {
-    options['model'] = Fault.where(causer_id: params[:user_id]).ordered.page(params[:page])
+  step ->(options, params:, current_user:, **) {
+    faults = if current_user.any_admin? || current_user.id == params[:user_id].to_i
+               Fault.where(causer_id: params[:user_id])
+             else
+               Fault.none
+             end
+    options['model'] = faults.ordered.page(params[:page])
   }
 end
