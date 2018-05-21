c9357eed63fabe2cd51b348a183e0ec108e56c1a
Vladimir Kochergin <kvn047@gmail.com>
2018-05-22 00:43:37 +1000

    Order report results

--- app/reports/quick_orders_report.rb
+++ app/reports/quick_orders_report.rb
@@ -3,7 +3,7 @@ class QuickOrdersReport < BaseReport
   def call
     result[:users] = []
     if (orders = QuickOrder.includes(:user).done.where(created_at: period)).any?
-      orders.group(:user_id).count('quick_orders.id').each_pair do |user_id, qty|
+      orders.order('count_quick_orders_id desc').group(:user_id).count('quick_orders.id').each_pair do |user_id, qty|
         user = User.find(user_id)
         result[:users] << {name: user.short_name, qty: qty}
       end
