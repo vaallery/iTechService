5a1b620cc31bf0659769be2f44e066ab72f3f1e6
Vladimir Kochergin <kvn047@gmail.com>
2018-07-01 22:33:16 +1000

    Add returns in Payments report

--- app/reports/payments_report.rb
+++ app/reports/payments_report.rb
@@ -2,14 +2,41 @@ class PaymentsReport < BaseReport
 
   def call
     result[:payment_kinds] = {}
-    payments = Payment.includes(:sale).where(sales: {date: period, status: 1, is_return: false})
-    result[:payments_sum] = payments.sum(:value)
-    result[:payments_qty] = payments.count
+    payments = Payment.includes(:sale).where(sales: {date: period, status: 1})
+
     Payment::KINDS.each do |kind|
       payments_of_kind = payments.where(kind: kind)
-      payments_hash = %w[card credit].include?(kind) ? payments_of_kind.group(:bank_id).sum(:value).map{|r| {bank: Bank.find(r[0]).try(:name), value: r[1]}} : payments_of_kind.map(&:attributes_hash)
-      result[:payment_kinds].store kind, {payments: payments_hash, sum: payments_of_kind.sum(:value), qty: payments_of_kind.count}
+      payments_hash = if %w[card credit].include?(kind)
+                        bank_payments_hash payments_of_kind
+                      else
+                        payments_of_kind.map(&:attributes_hash)
+                      end
+
+      sales_sum = payments_of_kind.sales.sum(:value)
+      returns_sum = payments_of_kind.returns.sum(:value)
+      result_sum = sales_sum - returns_sum
+
+      result[:payment_kinds].store kind, {
+        payments: payments_hash,
+        sales_sum: sales_sum,
+        returns_sum: returns_sum,
+        result_sum: result_sum,
+        qty: payments_of_kind.count
+      }
     end
+
+    result[:sales_sum] = payments.sales.sum(:value)
+    result[:returns_sum] = payments.returns.sum(:value)
+    result[:result_sum] = result[:sales_sum] - result[:returns_sum]
+    result[:payments_qty] = payments.count
     result
+  end
+
+  private
+
+  def bank_payments_hash(payments)
+    payments.group(:bank_id).sum(:value).map do |r|
+      {bank: Bank.find_by(id: r[0]).try(:name), value: r[1]}
+    end
   end
 end
