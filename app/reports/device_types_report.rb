e6bd0dfb871b5fbbe8522a66cff078c9dddc25b0
Vladimir Kochergin <kvn047@gmail.com>
2018-07-15 22:57:46 +1000

    Fix DeviceTypesReport

--- app/reports/device_types_report.rb
+++ app/reports/device_types_report.rb
@@ -7,17 +7,21 @@ class DeviceTypesReport < BaseReport
     result[:current_device_type] = DeviceType.find(device_type) if device_type.present?
     device_types = result[:current_device_type].present? ? result[:current_device_type].children : DeviceType.roots
     device_types.each do |device_type|
-      device_ids = []
+      job_ids = []
       if device_type.is_childless?
-        device_ids << device_type.service_jobs.where(created_at: period).map { |d| d.id }
+        job_ids += device_type.service_jobs.where(created_at: period).pluck(:id)
+        job_ids += ServiceJob.includes(item: {product: :device_type}).
+          where(created_at: period, products: {device_type_id: device_type.id}).pluck(:id)
       else
         device_type.descendants.each do |sub_device_type|
           if sub_device_type.is_childless?
-            device_ids << sub_device_type.service_jobs.where(created_at: period).map { |d| d.id }
+            job_ids += sub_device_type.service_jobs.where(created_at: period).pluck(:id)
+            job_ids += ServiceJob.includes(item: {product: :device_type}).
+              where(created_at: period, products: {device_type_id: sub_device_type.id}).pluck(:id)
           end
         end
       end
-      received_service_jobs = ServiceJob.where id: device_ids
+      received_service_jobs = ServiceJob.where id: job_ids
       qty_received = received_service_jobs.count
       qty_done = received_service_jobs.at_done.count
       qty_archived = received_service_jobs.at_archive.count
