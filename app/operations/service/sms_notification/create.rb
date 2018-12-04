191d20dad642638a0ab49aa69e9b6d317babafc6
Vladimir Kochergin <kvn047@gmail.com>
2018-12-04 21:20:24 +1000

    Fix defining phone number for sending sms

--- app/operations/service/sms_notification/create.rb
+++ app/operations/service/sms_notification/create.rb
@@ -8,10 +8,16 @@ module Service
 
     def prepare(service_job_id:, **params)
       service_job = ServiceJob.find(service_job_id)
-      phone_number = service_job.contact_phone || service_job.client.contact_phone || service_job.client.full_phone_number
+      phone_number = get_phone_number(service_job)
       SMSNotification.new params[:service_sms_notification].merge(phone_number: phone_number,
                                                                   sent_at: DateTime.current,
                                                                   sender: params[:current_user])
+    end
+
+    def get_phone_number(service_job)
+      phone_number = service_job.contact_phone
+      phone_number = service_job.client.full_phone_number if phone_number.nil? || phone_number == '-'
+      phone_number.gsub(/^7/, '8')
     end
 
     def create(sms_notification)
