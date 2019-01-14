388f0144b9a86da4b35c1042a88885be0f97713f
Vladimir Kochergin <kvn047@gmail.com>
2019-01-14 20:54:15 +1000

    Add location to `service jobs at done` report

--- app/reports/service_jobs_at_done_report.rb
+++ app/reports/service_jobs_at_done_report.rb
@@ -6,7 +6,7 @@ class ServiceJobsAtDoneReport < BaseReport
   def call
     result[:records] = []
     self.location_id ||= Location.select(:id).where(code: 'done').pluck(:id)
-    movements_to_done = HistoryRecord.joins('LEFT OUTER JOIN service_jobs ON service_jobs.id = history_records.object_id').service_jobs.movements_to(location_id).where(service_jobs: {location_id: location_id}).distinct.order(created_at: :asc).limit(100)
+    movements_to_done = HistoryRecord.joins('LEFT OUTER JOIN service_jobs ON service_jobs.id = history_records.object_id').service_jobs.movements_to(location_id).where(service_jobs: {location_id: location_id}).distinct.order(created_at: :asc)#.limit(100)
 
     movements_to_done.each do |movement|
       service_job = movement.object
