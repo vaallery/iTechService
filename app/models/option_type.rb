8147e6c723eefe59f1f6ff9c714903877477553d
Vladimir Kochergin <kvn047@gmail.com>
2019-03-23 22:26:08 +1000

    Fix validation of OptionValue

--- app/models/option_type.rb
+++ app/models/option_type.rb
@@ -1,7 +1,7 @@
 class OptionType < ActiveRecord::Base
   # default_scope { ordered }
   scope :ordered, -> { order 'option_types.position asc' }
-  has_many :option_values
+  has_many :option_values, inverse_of: :option_type
   accepts_nested_attributes_for :option_values, allow_destroy: true, reject_if: proc { |a| a[:name].blank? }
   validates :name, presence: true, uniqueness: true
   validates :code, uniqueness: {allow_blank: true}
