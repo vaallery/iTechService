b3fcae521cbe593c0abe7d4db771d880ee9d8b5d
Vladimir Kochergin <kvn047@gmail.com>
2019-05-16 21:18:05 +1000

    #30 Отчёт по продажам

--- app/reports/sales_report.rb
+++ app/reports/sales_report.rb
@@ -1,21 +1,58 @@
 class SalesReport < BaseReport
+  attr_accessor :department_id
 
   def call
-    result[:sales] = []
-    sales_sum = 0
-    sales_count = 0
-    discounts_sum = 0
     sales = Sale.sold_at(period).posted.order('date asc')
 
-    sales.selling.each do |sale|
+    if department_id.present?
+      sales = sales.joins(cash_shift: :cash_drawer).where(cash_drawers: {department_id: department_id})
+    end
+
+    data = {
+      goods: {
+        name: 'Товары',
+        count: 0,
+        sum: 0,
+        discount: 0,
+        details: []
+      }
+    }
+    total_sales_count = 0
+    total_sales_sum = 0
+    total_discounts_sum = 0
+
+    sales.selling.find_each do |sale|
       sale.sale_items.each do |sale_item|
         sale_item_sum = sale_item[:price] * sale_item[:quantity]
 
-        payment_type = sale.payment_kinds.map do |payment_kind|
-          I18n.t("payments.kinds.#{payment_kind}")
-        end.join(', ')
+        payment_type = sale.payment_kinds.map { |payment_kind| I18n.t("payments.kinds.#{payment_kind}") }.join(', ')
 
-        result[:sales] << {
+        if sale_item.is_service?
+          data_key = sale_item.device_task.task_id
+
+          if data.has_key?(data_key)
+            data[data_key][:count] += 1
+            data[data_key][:sum] += sale_item_sum
+            data[data_key][:discount] += sale_item.discount
+          else
+            data.store(data_key, {
+              name: sale_item.device_task.name,
+              count: 1,
+              sum: sale_item_sum,
+              discount: sale_item.discount,
+              details: []
+            })
+          end
+        else
+          data_key = :goods
+          data[data_key][:count] += sale_item.quantity
+          data[data_key][:sum] += sale_item_sum
+          data[data_key][:discount] += sale_item.discount
+        end
+
+        data[data_key][:details] << {
+          sale_id: sale.id,
+          job_id: sale.service_job&.id,
           time: sale.date,
           product: sale_item.name,
           features: sale_item.features_s,
@@ -30,15 +67,22 @@ class SalesReport < BaseReport
           user: sale.user_short_name
         }
 
-        sales_count = sales_count + sale_item.quantity
-        sales_sum = sales_sum + sale_item_sum
-        discounts_sum = discounts_sum + sale_item.discount
+        total_sales_count += sale_item.quantity
+        total_sales_sum += sale_item_sum
+        total_discounts_sum += sale_item.discount
       end
     end
 
-    result[:sales_count] = sales_count
-    result[:sales_sum] = sales_sum
-    result[:discounts_sum] = discounts_sum
-    result
+    result[:data] = data
+    result[:total_sales_count] = total_sales_count
+    result[:total_sales_sum] = total_sales_sum
+    result[:total_discounts_sum] = total_discounts_sum
+    self
+  end
+
+  private
+
+  def sales
+
   end
 end
