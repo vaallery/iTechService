237508f3c8d2e36a9fa53eee6d3f11c91982dae6
Vladimir Kochergin <kvn047@gmail.com>
2019-08-10 23:43:32 +1000

    Add experience to oncoming salaries list

--- app/helpers/salaries_helper.rb
+++ app/helpers/salaries_helper.rb
@@ -6,8 +6,30 @@ module SalariesHelper
       content_tag(:table, id: 'oncoming_salaries_list', class: 'table table-condensed table-hover') do
         users.map do |user|
           content_tag(:tr) do
-            time_text = user.upcoming_salary_date.today? ? t(:today)
-            : "#{t(:in_time)} #{distance_of_time_in_words(today, user.upcoming_salary_date)}"
+            time_text = if user.upcoming_salary_date.today?
+                          text = 'Отработал(а)'
+                          years, months = ((Time.now - user.hiring_date.to_time) / 1.month).divmod(12)
+
+                          if years > 0
+                            text << " #{years}"
+                            text << if years == 1 || (years > 20 && years.modulo(10) == 1)
+                                      ' год'
+                                    elsif (years < 5 || years > 20) && years.modulo(10).in?(2..4)
+                                      ' года'
+                                    else
+                                      ' лет'
+                                    end
+                          end
+
+                          months = months.to_i
+                          if months > 0
+                            text << " #{months} мес."
+                          end
+
+                          text
+                        else
+                          "#{t(:in_time)} #{distance_of_time_in_words(today, user.upcoming_salary_date)}"
+                        end
             content_tag(:td, time_text) +
             content_tag(:td, link_to(user.short_name, user_path(user))) +
             content_tag(:td, link_to(icon_tag(:money), finance_user_path(user), remote: true, class: 'new_salary_link', title: t('salaries.issue')))
