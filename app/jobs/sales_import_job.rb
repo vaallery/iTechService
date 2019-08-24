64c3a5ac9fc65488ba772ce20a4d97d435b564b2
Vladimir Kochergin <kvn047@gmail.com>
2019-08-24 22:33:33 +1000

    Fix SalesImportJob

--- app/jobs/sales_import_job.rb
+++ app/jobs/sales_import_job.rb
@@ -2,15 +2,18 @@ class SalesImportJob < ActiveJob::Base
   queue_as :default
 
   def perform(file)
-    import_logs = []
+    # import_logs = []
+    logs_file = File.new(Rails.root.join('log', "sales_import-#{Time.now.to_s(:number)}.log"), 'w')
     begin
       sheet = FileLoader.open_spreadsheet file
-      import_logs << ['Info', "Import started at #{Time.current.strftime('%Y.%m.%d %H:%M:%S')}"]
+      # import_logs << ['Info', "Import started at #{Time.current.strftime('%Y.%m.%d %H:%M:%S')}"]
+      logs_file << ['Info', "Import started at #{Time.current.strftime('%Y.%m.%d %H:%M:%S')}"]
       code_1c = device_type_s = sn = imei = date = qty = nil
       device_type = nil
       9.upto sheet.last_row-1 do |r|
         row = sheet.row r
-        import_logs << ['inverse', "#{r} #{'-'*140}"]
+        # import_logs << ['inverse', "#{r} #{'-'*140}"]
+        logs_file << ['inverse', "#{r} #{'-'*140}"]
         case row_type row[0], row[1]
           when :device_type
             # code_1c = RGXP_PRODUCT_CODE_AND_NAME.match(row[0])[1].to_i
@@ -18,37 +21,43 @@ class SalesImportJob < ActiveJob::Base
             code_1c = row[0].to_i
             device_type_s = row[1]
             unless (device_type = DeviceType.find_by_code_1c code_1c).present?
-              import_logs << ['important', "Device type '#{device_type_s}' [#{code_1c}] not found!!!"]
+              # import_logs << ['important', "Device type '#{device_type_s}' [#{code_1c}] not found!!!"]
+              logs_file << ['important', "Device type '#{device_type_s}' [#{code_1c}] not found!!!"]
             end
           when :device_1
             sn = RGXP_SERIAL_NUMBER.match(row[0])[1]
             imei = nil
-            import_logs << ['info', row[0]]
+            # import_logs << ['info', row[0]]
+            logs_file << ['info', row[0]]
           when :device_2
             sn = RGXP_SERIAL_NUMBER_IMEI.match(row[0])[1]
             imei = RGXP_SERIAL_NUMBER_IMEI.match(row[0])[2]
-            import_logs << ['info', row[0]]
+            # import_logs << ['info', row[0]]
+            logs_file << ['info', row[0]]
           when :date
             date = RGXP_DATE.match(row[0])[1]
-            qty = row[5].to_i
+            qty = row[7].to_i
             sale = ImportedSale.find_or_initialize_by serial_number: sn,
                                                       imei: imei,
                                                       sold_at: date.to_date,
                                                       device_type_id: device_type.try(:id),
                                                       quantity: qty
 
-            if sale.new_record? && sale.sold_at.to_date != date.to_date
+            if sale.new_record?
               if sale.save
-                import_logs << ['success', "#{sale.new_record? ? 'New' : 'Existing'} device #{device_type.try(:full_name)} [#{sale.serial_number}] sold_at: #{sale.sold_at}"]
+                # import_logs << ['success', "#{sale.new_record? ? 'New' : 'Existing'} device #{device_type.try(:full_name)} [#{sale.serial_number}] sold_at: #{sale.sold_at}"]
+                logs_file << ['success', "#{sale.new_record? ? 'New' : 'Existing'} device #{device_type.try(:full_name)} [#{sale.serial_number}] sold_at: #{sale.sold_at}"]
               else
-                import_logs << ['error', "Unable to save #{sale.inspect}: #{sale.errors.full_messages.join('. ')}"]
+                # import_logs << ['error', "Unable to save #{sale.inspect}: #{sale.errors.full_messages.join('. ')}"]
+                logs_file << ['error', "Unable to save #{sale.inspect}: #{sale.errors.full_messages.join('. ')}"]
               end
             end
         end
       end
-      import_logs << ['inverse', '-'*160]
+      # import_logs << ['inverse', '-'*160]
+      logs_file << ['inverse', '-'*160]
     end
-    ImportMailer.sales_import_log(import_logs).deliver_later
+    # ImportMailer.sales_import_log(import_logs).deliver_later
   end
 
   private
@@ -76,5 +85,4 @@ class SalesImportJob < ActiveJob::Base
       nil
     end
   end
-
 end
--- app/mailers/import_mailer.rb
+++ app/mailers/import_mailer.rb
@@ -2,12 +2,12 @@ class ImportMailer < ApplicationMailer
 
   def sales_import_log(import_logs)
     @import_logs = import_logs
-    mail to: 'kvn@itechdevs.com, oleg@itechstore.ru', subject: "Sales import log [#{l(Time.current, format: :long_d)}]"
+    mail to: 'kvn047@gmail.com', subject: "Sales import log [#{l(Time.current, format: :long_d)}]"
   end
 
   def product_import_log(import_log, time, type)
     @import_log = import_log
-    mail to: 'kvn@itechdevs.com, nastya@itechstore.ru', subject: t('mail.product_import_log.subject', type: type.to_s.humanize, time: l(time, format: :long))
+    mail to: 'kvn047@gmail.com', subject: t('mail.product_import_log.subject', type: type.to_s.humanize, time: l(time, format: :long))
   end
 
 end
