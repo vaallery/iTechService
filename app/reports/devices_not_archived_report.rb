a5a5f7747e9cb38ce5c8554c866949cf354b4069
Vladimir Kochergin <kvn047@gmail.com>
2019-08-31 21:49:19 +1000

    Modify DevicesNotArchivedReport

--- app/reports/devices_not_archived_report.rb
+++ app/reports/devices_not_archived_report.rb
@@ -1,12 +1,17 @@
 class DevicesNotArchivedReport < BaseReport
-
   def call
     service_jobs = ServiceJob.received_at(period)
     result[:received_qty] = service_jobs.count
     result[:archived_qty] = service_jobs.at_archive.count
     result[:not_archived_devices] = []
     service_jobs.not_at_archive.find_each do |service_job|
-      result[:not_archived_devices] << {received_at: service_job.received_at, ticket_number: service_job.ticket_number, job_id: service_job.id}
+      result[:not_archived_devices] << {
+        received_at: service_job.received_at,
+        ticket_number: service_job.ticket_number,
+        department: service_job.department_name,
+        receiver: service_job.user.short_name,
+        job_id: service_job.id
+      }
     end
     result[:not_archived_qty] = service_jobs.not_at_archive.count
     result
