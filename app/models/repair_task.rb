f2ef5febd1700d73c9fb2e29baeee431939daf52
Vladimir Kochergin <kvn047@gmail.com>
2019-09-01 20:26:36 +1000

    Fix Margin report

--- app/models/repair_task.rb
+++ app/models/repair_task.rb
@@ -3,11 +3,14 @@ class RepairTask < ActiveRecord::Base
   belongs_to :device_task
   belongs_to :store
   has_many :repair_parts, inverse_of: :repair_task
-  accepts_nested_attributes_for :repair_parts
+
   delegate :name, :repair_group, :is_positive_price, to: :repair_service, allow_nil: true
   delegate :price, to: :repair_service, prefix: true, allow_nil: true
   delegate :user, :service_job, :performer, :done, :done?, :pending?, :undone?, :department, to: :device_task, allow_nil: true
+
   attr_accessible :price, :repair_service_id, :device_task_id, :store_id, :repair_parts_attributes
+  accepts_nested_attributes_for :repair_parts
+
   validates :price, :repair_service, :store, presence: true
   validates :price, numericality: true
   validates_numericality_of :price, greater_than: 0, if: :is_positive_price
--- app/reports/margin_report.rb
+++ app/reports/margin_report.rb
@@ -3,9 +3,9 @@ class MarginReport < BaseReport
 
   def call
     result.deep_merge!({repair: {sum: 0, details: []}, service: {sum: 0, details: []}, sale: {sum: 0, details: []}, sum: 0})
-    sales = Sale.posted.sold_at(period).date_asc
+    sales = Sale.posted.selling.sold_at(period).date_asc
 
-    sales.selling.find_each do |sale|
+    sales.find_each do |sale|
       sale.sale_items.find_each do |sale_item|
         if sale_item.is_repair?
           kind = :repair
@@ -16,17 +16,20 @@ class MarginReport < BaseReport
         end
         details = []
         if sale_item.is_repair?
-          if (repair_parts = sale.try(:service_job).try(:repair_parts)).present?
-            repair_parts.each do |repair_part|
-              details.push repair_part.as_json(only: [:id], methods: [:name, :purchase_price]).transform_keys(&:to_sym)
-            end
+          sale_item.device_task&.repair_tasks&.each do |repair_task|
+            details << repair_task.as_json(only: [:id], methods: [:name, :price, :purchase_price, :margin])
+                         .transform_keys(&:to_sym)
+                         .merge(purchase_price: repair_task.parts_cost,
+                                quantity: repair_task.repair_parts.count,
+                                discount: '?')
+            result[kind][:sum] = result[kind][:sum] + repair_task.margin
           end
         else
           details.push sale_item.as_json(only: [:id], methods: [:name, :price, :quantity, :discount, :purchase_price, :margin]).transform_keys(&:to_sym)
+          result[kind][:sum] = result[kind][:sum] + (sale_item.margin * sale_item.quantity)
         end
-        result[kind][:details].concat details
-        result[kind][:sum] = result[kind][:sum] + (sale_item.margin * sale_item.quantity)
-        result[:sum] = result[:sum] + (sale_item.margin * sale_item.quantity)
+        result[kind][:details] += details
+        result[:sum] = result[:sum] + result[kind][:sum]
       end
     end
     result
