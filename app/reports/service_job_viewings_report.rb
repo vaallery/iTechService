ac1ab256dc75cadc7b76f806747f7db4503ff3ce
Vladimir Kochergin <kvn047@gmail.com>
2019-10-06 15:00:38 +1000

    Add sum to ServiceJobViewingsReport

--- app/reports/service_job_viewings_report.rb
+++ app/reports/service_job_viewings_report.rb
@@ -1,5 +1,5 @@
 class ServiceJobViewingsReport < BaseReport
-  ReportRecord = Struct.new(:time, :job_id, :ticket_number, :viewer)
+  ReportRecord = Struct.new(:time, :job_id, :ticket_number, :viewer, :sum)
 
   attr_accessor :department_id
 
@@ -7,14 +7,14 @@ class ServiceJobViewingsReport < BaseReport
     location_id = Location.where(code: 'done', department_id: department_id).first.id
     viewings = ServiceJobViewing.includes(:service_job, :user)
                  .where(time: period, service_jobs: {location_id: location_id})
-    records = []
 
     result[:records] = viewings.map do |viewing|
       ReportRecord.new(
                     viewing.time,
                     viewing.service_job_id,
                     viewing.service_job.ticket_number,
-                    viewing.user_presentation
+                    viewing.user_presentation,
+                    viewing.service_job.tasks_cost
       )
     end
   end
