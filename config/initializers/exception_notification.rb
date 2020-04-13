95c27b7818f9c6f4c2ac26521e85eed46027d028
Vladimir Kochergin <kvn047@gmail.com>
2020-04-13 14:01:40 +1000

    Config

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,7 +1,5 @@
 require 'exception_notification/rails'
 
-
-
 ExceptionNotification.configure do |config|
   # Ignore additional exception types.
   # ActiveRecord::RecordNotFound, AbstractController::ActionNotFound and ActionController::RoutingError are already added.
@@ -18,38 +16,7 @@ ExceptionNotification.configure do |config|
   # Email notifier sends notifications by email.
   config.add_notifier :email, {
     email_prefix: '[ERROR] ',
-    sender_address: %{"iTechService #{ENV['DEPARTMENT_NAME']}" <#{ENV['EN_SMTP_LOGIN']}>},
+    sender_address: %{"iTechService" <#{ENV['EN_SMTP_LOGIN']}>},
     exception_recipients: ENV['EN_RECIPIENT']
-    # delivery_method: :smtp,
-    # smtp_settings: {
-    #   domain: ENV['EN_SMTP_DOMAIN'],
-    #   address: ENV['EN_SMTP_SERVER'],
-    #   port: ENV['EN_SMTP_PORT'],
-    #   user_name: ENV['EN_SMTP_LOGIN'],
-    #   password: ENV['EN_SMTP_PASSWORD'],
-    #   authentication: ENV['EN_SMTP_AUTHENTICATION'],
-    #   enable_starttls_auto: true,
-    #   tls: false
-    # }
   }
-
-  # Campfire notifier sends notifications to your Campfire room. Requires 'tinder' gem.
-  # config.add_notifier :campfire, {
-  #   :subdomain => 'my_subdomain',
-  #   :token => 'my_token',
-  #   :room_name => 'my_room'
-  # }
-
-  # HipChat notifier sends notifications to your HipChat room. Requires 'hipchat' gem.
-  # config.add_notifier :hipchat, {
-  #   :api_token => 'my_token',
-  #   :room_name => 'my_room'
-  # }
-
-  # Webhook notifier sends notifications over HTTP protocol. Requires 'httparty' gem.
-  # config.add_notifier :webhook, {
-  #   :url => 'http://example.com:5555/hubot/path',
-  #   :http_method => :post
-  # }
-
 end
