abae99624690b6a6b45a3b9acdd4ca6676f83572
Vladimir Kochergin <kvn047@gmail.com>
2020-05-08 20:14:31 +1000

    Fix

--- app/policies/purchase_policy.rb
+++ app/policies/purchase_policy.rb
@@ -2,4 +2,10 @@ class PurchasePolicy < DocumentPolicy
   def manage?
     superadmin? || able_to?(:manage_stocks)
   end
+
+  def print_barcodes?; manage? end
+
+  def revaluate_products?; manage? end
+
+  def move_items?; manage? end
 end
