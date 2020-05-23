908cf20bf2b390735748c3a004eaf74f358f352d
Vladimir Kochergin <kvn047@gmail.com>
2020-05-23 15:25:46 +1000

    Fix

--- app/models/service/free_task.rb
+++ app/models/service/free_task.rb
@@ -7,11 +7,11 @@ module Service
       name
     end
 
-    def possible_performers
+    def possible_performers(department = Department.current)
       return User.none if code.nil?
 
-      location_ids = Location.code_start_with(code).pluck(:id)
-      location_ids.any? ? User.active.where(location_id: location_ids) : User.none
+      locations = Location.in_department(department).code_start_with(code)
+      locations.any? ? User.active.where(location_id: locations) : User.none
     end
   end
 end
