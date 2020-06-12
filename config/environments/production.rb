fcee1e873627daeb7237ad58a4e62bd12d6afddd
Vladimir Kochergin <kvn047@gmail.com>
2020-06-12 22:54:58 +1000

    Config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: ENV['SERVER_HOST'] }
-  # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :sendmail
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
