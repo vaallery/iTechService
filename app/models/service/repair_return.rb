04441298a438210a8572224b3687daf0368c18e1
Vladimir Kochergin <kvn047@gmail.com>
2020-06-13 11:22:12 +1000

    Fix RepairReturn

--- app/models/service/repair_return.rb
+++ app/models/service/repair_return.rb
@@ -2,6 +2,8 @@ module Service
   class RepairReturn < ActiveRecord::Base
     self.table_name = 'service_repair_returns'
 
+    scope :in_department, ->(department) { where(service_job_id: ServiceJob.in_department(department)) }
+
     belongs_to :service_job
     belongs_to :performer, class_name: 'User'
     delegate :repair_parts, :sale, :department, to: :service_job, allow_nil: true
