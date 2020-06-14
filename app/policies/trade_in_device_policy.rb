df5a5f88b4150d516bd8eee3bd5a2d2ed2316143
Vladimir Kochergin <kvn047@gmail.com>
2020-06-14 18:36:21 +1000

    Fix trade-in access

--- app/policies/trade_in_device_policy.rb
+++ app/policies/trade_in_device_policy.rb
@@ -14,4 +14,16 @@ class TradeInDevicePolicy < BasePolicy
   def index_unconfirmed?
     manage?
   end
+
+  class Scope < Scope
+    def resolve
+      return scope.all if user.superadmin? || user.able_to?(:manage_trade_in)
+
+      if scope.column_names.include?('department_id')
+        scope.where(department_id: user.department_id)
+      else
+        scope.in_department(user.department_id)
+      end
+    end
+  end
 end
