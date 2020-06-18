658e5e05ce9787decf7b49a5387a155f60274578
Vladimir Kochergin <kvn047@gmail.com>
2020-06-18 21:26:55 +1000

    Fx

--- app/policies/service/feedback_policy.rb
+++ app/policies/service/feedback_policy.rb
@@ -13,13 +13,13 @@ module Service
     class Scope < Scope
       def resolve
         if user.superadmin? || user.able_to?(:view_feedback_notifications)
-          location = Location.where.not(code: %w[archive done special])
+          locations = Location.where.not(code: %w[archive done special])
         elsif user.able_to?(:view_feedbacks_in_city)
-          location = Location.in_city(user.city).where.not(code: %w[archive done special])
+          locations = Location.in_city(user.city).where.not(code: %w[archive done special])
         else
-          location = user.location
+          locations = user.location
         end
-        scope.where(service_job_id: ServiceJob.located_at(location))
+        scope.where(service_job_id: ServiceJob.located_at(locations))
       end
     end
   end
