4bf89b59e8b79e1315253ef8436b8ab46bd66580
Vladimir Kochergin <kvn047@gmail.com>
2020-12-19 20:04:18 +1000

    Fix MediaMenu db connection

--- app/models/media_menu/base_record.rb
+++ app/models/media_menu/base_record.rb
@@ -1,7 +1,10 @@
 module MediaMenu
   class BaseRecord < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection adapter: 'sqlite3', database: Setting.meda_menu_database unless Setting.meda_menu_database.nil?
+
+    if ActiveRecord::Base.connection.table_exists?(:settings) && Setting.meda_menu_database.present?
+      establish_connection(adapter: 'sqlite3', database: Setting.meda_menu_database)
+    end
 
     def database_folder
       @database_folder ||= File.dirname Setting.meda_menu_database
