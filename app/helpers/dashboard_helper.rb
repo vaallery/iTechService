ec80f3731e8d87477fa4fc67b2a4d7e916d85493
Valeriy <vmahanov@gmail.com>
2021-01-30 15:44:02 +0300

    Дублировать в список джобов информацию из полей

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,6 @@
-module DashboardHelper
+# frozen_string_literal: true
 
+module DashboardHelper
   def service_job_information_content(service_job)
     if service_job.present?
       (tag(:hr) +
@@ -15,12 +16,12 @@ module DashboardHelper
 
   def link_to_edit_service_job(service_job)
     link_to icon_tag(:edit), edit_service_job_path(service_job), class: 'btn btn-small', remote: true
-            #, disabled: !is_movable_device?(service_job)
+    # , disabled: !is_movable_device?(service_job)
   end
 
   def link_to_edit_device_task(device_task)
     link_to icon_tag('edit'), edit_device_task_path(device_task), class: 'btn btn-small', remote: true,
-            disabled: !policy(device_task).edit?
+                                                                  disabled: !policy(device_task).edit?
   end
 
   def is_actual_service_job?(service_job)
@@ -37,62 +38,81 @@ module DashboardHelper
   end
 
   def service_job_row_tag(service_job)
-    content_tag(:tr, class: 'info service_job_row', data: {service_job_id: service_job.id}) do
+    content_tag(:tr, class: 'info service_job_row', data: { service_job_id: service_job.id }) do
       content_tag(:td, device_movement_information_tag(service_job), class: 'device_movement_column') +
         content_tag(:td, class: 'device_task_column') do
-        content_tag(:span, service_job.progress, class: "device_tasks_toggle #{progress_badge_class_for_service_job(service_job)}") +
-        link_to(service_job.presentation, service_job_path(service_job)) +
-        tag(:br, false) +
-        content_tag(:span, class: 'device_ticket_number') do
-          "#{ServiceJob.human_attribute_name(:ticket_number)}: #{service_job.ticket_number}"
+          content_tag(:span, service_job.progress,
+                      class: "device_tasks_toggle #{progress_badge_class_for_service_job(service_job)}") +
+            link_to(service_job.presentation, service_job_path(service_job)) +
+            service_job_row_tag_device_attribute(service_job,
+                                                 %i[ticket_number claimed_defect device_condition client_comment
+                                                    type_of_work estimated_cost_of_repair])
+        end +
+        content_tag(:td, class: 'client_comment_column') do
+          (if service_job.client.present?
+             link_to(service_job.client_short_name,
+                     client_path(service_job.client))
+           else
+             '-'
+           end).html_safe +
+            " #{contact_phones_for(service_job)}".html_safe +
+            tag(:br, false) +
+            service_job.note
+        end +
+        content_tag(:td, class: 'device_task_action_column') do
+          notes_icon_name = service_job.device_notes.exists? ? 'file-text-alt' : 'file-alt'
+          link_to_edit_service_job(service_job) +
+            link_to(glyph(notes_icon_name), service_job_device_notes_path(service_job),
+                    class: 'device_notes-button btn btn-small', remote: true) +
+            service_job.decorate.subscription_button +
+            button_to_set_keeper_of_device(service_job)
         end
-      end +
-      content_tag(:td, class: 'client_comment_column') do
-        (service_job.client.present? ? link_to(service_job.client_short_name, client_path(service_job.client)) : '-').html_safe +
-        " #{contact_phones_for(service_job)}".html_safe +
-        tag(:br, false) +
-        service_job.note
-      end +
-      content_tag(:td, class: 'device_task_action_column') do
-        notes_icon_name = service_job.device_notes.exists? ? 'file-text-alt' : 'file-alt'
-        link_to_edit_service_job(service_job) +
-        link_to(glyph(notes_icon_name), service_job_device_notes_path(service_job), class: 'device_notes-button btn btn-small', remote: true) +
-        service_job.decorate.subscription_button +
-        button_to_set_keeper_of_device(service_job)
-      end
     end
   end
 
+  def service_job_row_tag_device_attribute(service_job, attributes = [])
+    res = ActiveSupport::SafeBuffer.new
+    attributes.each do |attr|
+      res += tag(:br, false)
+      res += content_tag(:strong, class: "device_#{attr}") do
+        "#{ServiceJob.human_attribute_name(attr)}: "
+      end
+      res += content_tag(:span) { service_job.send(attr) }
+    end
+    res
+  end
+
   def ready_service_job_row_tag(service_job)
-    content_tag(:tr, class: 'service_job_row', data: {service_jobs_id: service_job.id}) do
+    content_tag(:tr, class: 'service_job_row', data: { service_jobs_id: service_job.id }) do
       content_tag(:td, device_movement_information_tag(service_job), class: 'device_movement_column') +
-      content_tag(:td) do
-        link_to(service_job.presentation, service_job_path(service_job)) +
-        tag(:br, false) +
-        content_tag(:span, class: 'device_ticket_number') do
-          "#{ServiceJob.human_attribute_name(:ticket_number)}: #{service_job.ticket_number}"
-        end
-      end +
-      content_tag(:td) do
-        link_to(service_job.client_presentation, client_path(service_job.client)) +
-        tag(:br, false) +
-        service_job.comment
-      end +
-      content_tag(:td, service_job.done_at.present? ? l(service_job.done_at, format: :date_time) : '-') +
-      content_tag(:td, link_to_edit_service_job(service_job))
+        content_tag(:td) do
+          link_to(service_job.presentation, service_job_path(service_job)) +
+            tag(:br, false) +
+            content_tag(:span, class: 'device_ticket_number') do
+              "#{ServiceJob.human_attribute_name(:ticket_number)}: #{service_job.ticket_number}"
+            end
+        end +
+        content_tag(:td) do
+          link_to(service_job.client_presentation, client_path(service_job.client)) +
+            tag(:br, false) +
+            service_job.comment
+        end +
+        content_tag(:td, service_job.done_at.present? ? l(service_job.done_at, format: :date_time) : '-') +
+        content_tag(:td, link_to_edit_service_job(service_job))
     end
   end
 
   def device_task_row_tag(device_task)
-    content_tag(:tr, class: "device_task_row #{device_task.done_s} #{'actual' if is_actual_task?(device_task)}", data: {device_task_id: device_task.id, service_job_id: device_task.service_job.id, task_id: device_task.task.id}) do
+    content_tag(:tr, class: "device_task_row #{device_task.done_s} #{'actual' if is_actual_task?(device_task)}",
+                     data: { device_task_id: device_task.id, service_job_id: device_task.service_job.id, task_id: device_task.task.id }) do
       content_tag(:td, nil) +
-      content_tag(:td, device_task.task_name) +
-      content_tag(:td) do
-        content_tag(:span, h(device_task.comment)) +
-        tag(:br) +
-        content_tag(:strong, h(device_task.user_comment), class: 'user_comment').html_safe
-      end +
-      content_tag(:td, policy(device_task).edit? ? link_to_edit_device_task(device_task) : nil)
+        content_tag(:td, device_task.task_name) +
+        content_tag(:td) do
+          content_tag(:span, h(device_task.comment)) +
+            tag(:br) +
+            content_tag(:strong, h(device_task.user_comment), class: 'user_comment').html_safe
+        end +
+        content_tag(:td, policy(device_task).edit? ? link_to_edit_device_task(device_task) : nil)
     end.html_safe
   end
 
@@ -101,8 +121,8 @@ module DashboardHelper
       details.map do |detail|
         content_tag(:tr) do
           content_tag(:td, detail[:date].strftime('%d.%m.%y')) +
-          content_tag(:td, detail[:amount]) +
-          content_tag(:td, detail[:comment])
+            content_tag(:td, detail[:amount]) +
+            content_tag(:td, detail[:comment])
         end
       end.join.html_safe
     end.gsub('\n', '')
@@ -113,8 +133,8 @@ module DashboardHelper
       details.map do |detail|
         content_tag(:tr) do
           content_tag(:td, detail[:date].strftime('%d.%m.%y')) +
-          content_tag(:td, detail[:value]) +
-          content_tag(:td, detail[:object])
+            content_tag(:td, detail[:value]) +
+            content_tag(:td, detail[:object])
         end
       end.join.html_safe
     end.gsub('\n', '')
