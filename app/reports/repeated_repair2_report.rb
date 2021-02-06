c89a184407f21c5b91d355632f371a739cb7bd9e
Valeriy <vmahanov@gmail.com>
2021-02-06 20:09:41 +0300

    RepeatedRepairReport - fix

--- app/reports/repeated_repair2_report.rb
+++ app/reports/repeated_repair2_report.rb
@@ -3,22 +3,22 @@
 class RepeatedRepair2Report < BaseReport
   def call
     sql = <<-SQL
-      select s1.*, s2.c c1, s3.c c2
+      select s1.*, s2.c c1
       from service_jobs s1
-      left join (
-          SELECT serial_number, count(*) c FROM service_jobs
-          where serial_number is not null and serial_number <> '' and serial_number <> '-'
-          GROUP BY serial_number HAVING count(*) > 1
-          ) s2 on s1.serial_number = s2.serial_number
-      left join (
-          SELECT imei, count(*) c FROM service_jobs
-          where imei is not null and imei <> '' and imei <> '-'
-          GROUP BY imei HAVING count(*) > 1
-      ) s3 on s1.imei = s3.imei
-      where ((s1.serial_number is not null and s1.serial_number <> '' and s1.serial_number <> '?') or (s1.imei is not null and s1.imei <> '' and s1.imei <> '?'))
-        and ( s2.c > 1 or s3.c > 1) and created_at >= '#{Date.parse(start_date).iso8601}' and created_at <= '#{Date.parse(end_date).iso8601}'
-        #{"'location_id = ' #{department.id}" if department}
-      order by s1.imei, s1.serial_number;
+               left join (
+          SELECT item_id, count(*) c FROM service_jobs
+          where item_id is not null
+            and created_at >= '#{Date.parse(start_date).iso8601}'
+            and created_at <= '#{Date.parse(end_date).iso8601}'
+            #{"'and location_id = ' #{department.id}" if department}
+          GROUP BY item_id HAVING count(*) > 1
+      ) s2 on s1.item_id = s2.item_id
+
+      where s1.item_id is not null and s2.c > 1
+        and created_at >= '#{Date.parse(start_date).iso8601}'
+        and created_at <= '#{Date.parse(end_date).iso8601}'
+        #{"'and location_id = ' #{department.id}" if department}
+      order by s1.item_id desc
     SQL
     result[:jobs] = ServiceJob.find_by_sql sql
     result
--- app/reports/repeated_repair_report.rb
+++ app/reports/repeated_repair_report.rb
@@ -3,7 +3,6 @@
 class RepeatedRepairReport < BaseReport
   def call
     jobs = ServiceJob.select('item_id, device_type_id, serial_number, imei, count(*) qty')
-                     .where('imei is not null and imei <> '' or serial_number is not null')
                      .group('item_id, device_type_id, serial_number, imei').having('count(*) > 1')
     jobs = jobs.in_department(department) if department
     jobs = jobs.where(created_at: start_date..end_date)
